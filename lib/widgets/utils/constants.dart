7684ea78a7f518a0dc36d9304f337035566864b4
Hamid <HamidSaliqa>
2025-06-15 16:46:34 +0430

    برای نگهداری لیستی از کتگوری ها

---
+++ lib/widgets/utils/constants.dart
@@ -0,0 +1,13 @@
+// lib/utils/constants.dart
+
+/// ثابت‌های سراسری پروژه
+abstract class Constants {
+  /// لیست دسته‌بندی‌های ثابت
+  static const List<String> categories = [
+    'Electronics',
+    'Clothing',
+    'Food',
+    'Books',
+    'Sports',
+  ];
+}
