371df6ae6f86ef1da45e3b9bbf719373df1b4c9d
Hamid <HamidSaliqa>
2025-06-15 16:45:53 +0430

    برای مدیریت فاصله ها عمودی یکدست

---
+++ lib/widgets/utils/app_spacing.dart
@@ -0,0 +1,13 @@
+// lib/utils/app_spacing.dart
+
+import 'package:flutter/widgets.dart';
+
+/// ثابت‌های فاصله‌ی عمودی سراسری
+abstract class AppSpacing {
+  static const v8  = SizedBox(height: 8);
+  static const v12 = SizedBox(height: 12);
+  static const v16 = SizedBox(height: 16);
+  static const v20 = SizedBox(height: 20);
+  static const v24 = SizedBox(height: 24);
+  static const v32 = SizedBox(height: 32);
+}
