e54cf280394441112b2a744128991f38a96103d3
Hamid <HamidSaliqa>
2025-06-17 22:18:49 +0430

    convex_bottom_bar اضافه نمودن

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,23 +1,27 @@
 // lib/screens/home_screen.dart
 
 import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import '../data/db_helper.dart';
 import '../models/product.dart';
-import '../screens/add_product_screen.dart';
-import '../screens/details_screen.dart';
 import '../widgets/home/search_bar_custom.dart';
 import '../widgets/home/header_with_button.dart';
 import '../widgets/home/product_card.dart';
-import '../widgets/utils/app_spacing.dart';
+import 'CustomerScreen.dart';
+import 'add_product_screen.dart';
+import 'details_screen.dart';
 
-/// صفحهٔ اصلی: نمایش لیست محصولات از SQLite
+/// صفحهٔ اصلی با ConvexAppBarا
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
-  @override State<HomeScreen> createState() => _HomeScreenState();
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Product> products = [];
+  int _selectedIndex = 0;           // شاخص تب فعال
+  List<Product> _products = [];     // لیست محصولات
 
   @override
   void initState() {
@@ -25,56 +29,115 @@ class _HomeScreenState extends State<HomeScreen> {
     _loadProducts();
   }
 
+  /// بارگذاری محصولات از دیتابیس
   Future<void> _loadProducts() async {
     final list = await DbHelper.getProducts();
-    setState(() => products = list);
+    setState(() => _products = list);
+  }
+
+  /// هندل سوییچ تب‌ها
+  void _onTabTapped(int idx) {
+    setState(() => _selectedIndex = idx);
   }
 
   @override
   Widget build(BuildContext context) {
+    // صفحات را هر بار اینجا بساز تا پویا باشد
+    final pages = <Widget>[
+      _HomePageBody(
+        products: _products,
+        reload: _loadProducts,
+      ),
+      const CustomerScreen(),
+    ];
+
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              AppSpacing.v12,
-              SearchBarCustom(
-                onTextChanged: (t) {},
-                onFilterPressed: () {},
-              ),
-              AppSpacing.v24,
-              HeaderWithButton(
-                onAddPressed: () {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (_) => const AddProductScreen()))
-                      .then((_) => _loadProducts());
+      body: pages[_selectedIndex],
+
+      // نوار پایین ConvexAppBar با style.react
+      bottomNavigationBar: ConvexAppBar(
+        style: TabStyle.react,
+        backgroundColor: Colors.white,
+        activeColor: Colors.blue,
+        color: Colors.grey,
+        elevation: 8,
+        initialActiveIndex: _selectedIndex,
+        items: const [
+          TabItem(icon: Icons.home, title: 'Home'),
+          TabItem(icon: Icons.person, title: 'Customers'),
+        ],
+        onTap: _onTabTapped,
+      ),
+    );
+  }
+}
+
+/// بدنهٔ صفحهٔ Home
+class _HomePageBody extends StatelessWidget {
+  final List<Product> products;
+  final Future<void> Function() reload;
+
+  const _HomePageBody({
+    Key? key,
+    required this.products,
+    required this.reload,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            const SizedBox(height: 12),
+
+            // ۱) نوار جستجو
+            SearchBarCustom(
+              onTextChanged: (text) { /* TODO: فیلتر */ },
+              onFilterPressed: () { /* TODO: فیلتر */ },
+            ),
+            const SizedBox(height: 24),
+
+            // ۲) هدر با دکمه Add
+            HeaderWithButton(
+              onAddPressed: () {
+                Navigator.of(context)
+                    .push(
+                  MaterialPageRoute(builder: (_) => const AddProductScreen()),
+                )
+                    .then((_) => reload());
+              },
+            ),
+            const SizedBox(height: 16),
+
+            // ۳) لیست محصولات
+            Expanded(
+              child: products.isEmpty
+                  ? const Center(child: Text('No products available'))
+                  : ListView.separated(
+                physics: const BouncingScrollPhysics(),
+                itemCount: products.length,
+                separatorBuilder: (_, __) => const SizedBox(height: 16),
+                itemBuilder: (ctx, i) {
+                  final p = products[i];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(
+                        MaterialPageRoute(
+                          builder: (_) => DetailsScreen(product: p),
+                        ),
+                      )
+                          .then((_) => reload());
+                    },
+                    child: ProductCard(product: p),
+                  );
                 },
               ),
-              AppSpacing.v16,
-              Expanded(
-                child: products.isEmpty
-                    ? const Center(child: Text('No products available'))
-                    : ListView.separated(
-                  physics: const BouncingScrollPhysics(),
-                  itemCount: products.length,
-                  separatorBuilder: (_, __) => AppSpacing.v16,
-                  itemBuilder: (ctx, i) {
-                    final p = products[i];
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.of(context)
-                            .push(MaterialPageRoute(builder: (_) => DetailsScreen(product: p)))
-                            .then((_) => _loadProducts());
-                      },
-                      child: ProductCard(product: p),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
